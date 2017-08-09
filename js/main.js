6efd5ef29e8663a1e1906dd1a9110ca976a0c96f
jais210 <jakeline_210@hotmail.com>
2017-08-09 10:52:36 -0500

    html, js, y css archivos crados(vacíos aún)

---
+++ js/main.js
@@ -0,0 +1,84 @@
+var miLista = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "illo expedita consequatur quia in",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "quo adipisci enim quam ut ab",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "molestiae perspiciatis ipsa",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "illo est ratione doloremque quia maiores aut",
+    completed: true
+  }
+];
+
+function elementoEnLista(){
+  
+str = "<ul>";
+for (var i = 0; i < miLista.length; i++) {
+  var li = document.createElement("li");
+  var span = document.createElement("span");
+
+  span.className = "close";
+  span.textContent = miLista[i].title;
+  li.appendChild(span);
+  lista.appendChild(li);
+  str += "<li><span class='close'>" + miLista[i].title + "</span></li>";
+
+}
+str += "</ul>";
+  lista.innerHTML = str;
+}
+
+function mostrarTarea (){
+
+}
